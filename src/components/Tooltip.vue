2b765ff358bed2af8a5a49b958fbf6b47e23b69f
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-11-30 22:07:44 -0500

    Scaffold the Tooltip component

--- src/components/Tooltip.vue
+++ src/components/Tooltip.vue
@@ -1,3 +1,36 @@
 <template>
-  <h1>Testing</h1>
+  <div class="container">
+    <v-row no-gutters>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+      <v-col>
+        <v-sheet class="pa-1 ma-1">Event</v-sheet>
+      </v-col>
+    </v-row>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  margin: 1rem;
+}
+</style>
--- src/views/TimeTableView.vue
+++ src/views/TimeTableView.vue
@@ -11,7 +11,11 @@ export default {
 </script>
 
 <template>
+<<<<<<< HEAD
   <v-sheet :elevation="10" :height="600" :width="1200">
+=======
+  <v-sheet :elevation="10" :height="650" :width="1280">
+>>>>>>> 8b46f77 (Restructured the TimeTableView into Calendar and Tooltip components)
     <Tooltip />
     <Calendar />
   </v-sheet>
