c9d423587d6387ac185cdd9c952d49a5cbe5218f
Tri Nguyen <nghaminhtri@gmail.com>
2023-11-30 21:39:31 -0500

    Set up Firebase

---
+++ firebase/init.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import {getFirestore} from "firebase/firestore"
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCTZsjKTY_8VgXEzAgOLIJqCCIV4mxeBWk",
+  authDomain: "calendar-f850e.firebaseapp.com",
+  projectId: "calendar-f850e",
+  storageBucket: "calendar-f850e.appspot.com",
+  messagingSenderId: "843229410868",
+  appId: "1:843229410868:web:9d37be74566eeaf4b37c61",
+  measurementId: "G-5HSCGJY3BT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const db = getFirestore()
+export default db 
